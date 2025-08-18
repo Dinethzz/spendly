c477417e2d0367d170aa52eb45d46025d66fb10e
Dineth Bandara <131736551+Dinethzz@users.noreply.github.com>
2025-08-18 23:17:01 +0530

    Expences

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,11 @@
 import 'package:expenz/constants/colors.dart';
+import 'package:expenz/models/expence_model.dart';
 import 'package:expenz/screens/add_new_screen.dart';
 import 'package:expenz/screens/budget_screen.dart';
 import 'package:expenz/screens/home_screen.dart';
 import 'package:expenz/screens/profile_screen.dart';
 import 'package:expenz/screens/transaction_screen.dart';
+import 'package:expenz/services/expence_service.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,13 +17,42 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentPageIndex = 0;
+
+  List<Expence> expenceList = [];
+  //function to fetch expences
+  void fetchAllExpences() async {
+    List<Expence> loadedExpences = await ExpenseService().loadExpences();
+    setState(() {
+      expenceList = loadedExpences;
+    });
+  }
+
+  //function to add a new expence
+  void addNewExpence(Expence newExpence) {
+    ExpenseService().saveExpences(newExpence, context);
+
+    setState(() {
+      expenceList.add(newExpence);
+    });
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setState(() {
+      fetchAllExpences();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //screen list
     final List<Widget> screens = [
       const HomeScreen(),
       const TransactionScreen(),
-      const AddNewScreen(),
+      AddNewScreen(
+        addExpence: addNewExpence
+      ),
       const BudgetScreen(),
       const ProfileScreen(),
     ];
