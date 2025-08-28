acbc62ae2923b2d842b935c697fbbe0456fbb1ad
Dineth Bandara <131736551+Dinethzz@users.noreply.github.com>
2025-08-28 23:17:16 +0530

    ui update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:expenz/constants/colors.dart';
+import 'package:expenz/constants/const_values.dart';
 import 'package:expenz/services/user_service.dart';
 import 'package:expenz/widgets/income_expence_card.dart';
+import 'package:expenz/widgets/line_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -34,12 +36,15 @@ class _HomeScreenState extends State<HomeScreen> {
       print("Error loading user data: $e");
     }
   }
+  
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 height: MediaQuery.of(context).size.height * 0.25,
@@ -93,6 +98,23 @@ class _HomeScreenState extends State<HomeScreen> {
                         IncomeExpenceCard(title: "Expence", amount: 1200, imageUrl: "assets/images/expence.png", bgcolor: kRed),
                       ],
                     )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(kDefaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Spend Frequency",
+                      style: TextStyle(
+                        color: kBlack,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    LineChartSample(),
                   ],
                 ),
               )
